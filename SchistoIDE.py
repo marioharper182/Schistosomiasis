867e27840780fd3f67a42d8b9cb1ead2798baf0b
unknown <marioharper182@gmail.com>
2015-02-20 13:27:55 -0700

    Fixes

--- SchistoIDE.py
+++ SchistoIDE.py
@@ -6,6 +6,7 @@ import numpy
 from scipy.stats import linregress
 import math
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
 # Variables definition
@@ -16,7 +17,7 @@ dx = 2*x1/np
 D = .01
 
 mu = 0.0002
-delta = 0.05
+deltas = [0.01, .05, .1]
 h = 100
 f = 0.00005
 b = .0005
@@ -40,19 +41,28 @@ critvalueH = []
 CritX = array(critvalueX)
 CritH = array(critvalueH)
 
+xlist = []
+Hinitial = []
+H1 = []
+H2 = []
 
-for delta in [0.01, .05, .1]:
+for delta in deltas:
+    count = 0
     if delta == .01:
         Hcrit = Hcritlist[0]
     if delta == .05:
         Hcrit = Hcritlist[1]
+        count = count + 1
     if delta == .1:
         Hcrit = Hcritlist[2]
+        count = count + 1
     #Discretization of initial distribution
     x = []
     for i in range(0,np-1):
         newrange = -x1 + i*dx
         x.append(newrange)
+        if count == 0:
+            xlist.append(newrange)
     x = array(x)
 
     I0 = array([0] * 255)
@@ -84,6 +94,13 @@ for delta in [0.01, .05, .1]:
         I0 = array(fftshift(ifft(fK*fI0)).real)
 
         H = (1-mu)*H0+c*I0
+        for i in H:
+            if count == 0:
+                Hinitial.append(i)
+            if count == 1:
+                H1.append(i)
+            if count == 2:
+                H2.append(i)
         I = (1-delta)*I0+b*((s-I0)*H0**2)/(1+H0)
 
         for i in range(128, len(x)):
@@ -98,7 +115,7 @@ for delta in [0.01, .05, .1]:
             plt.xlabel('Distance')
             plt.ylabel('Worms (per host)')
             plt.title('Schistosomiasis: diffusion + advection')
-            plt.show()
+            # plt.show()
 
         I0 = I
         H0 = H
@@ -116,7 +133,7 @@ for delta in [0.01, .05, .1]:
     InstantlistH = (InstantlistH)
     print((Instantlistx,InstantlistH))
 
-    plt.plot(Instantlistx, InstantlistH, 'ro', linestyle = '-')
+    plt.plot(Instantlistx, InstantlistH, 'b', linestyle = '-')
     plt.xlabel('Spatial Coordinate of Wave Front')
     plt.ylabel('Time to infection')
     plt.title('speed graph')
@@ -128,7 +145,31 @@ for delta in [0.01, .05, .1]:
     Regressionstats = linregress(RegressX,RegressY)
     print(Regressionstats)
 
+#### 3D Plot ####
+fig = plt.figure()
+# ax = fig.gca(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
+
+x = xlist
+y = deltas
+
+X,Y = meshgrid(x, y)
+Z =array([Hinitial, H1, H2])
+
+xs = [i for i in X]
+ys = [i for i in Y]
+zs = [i for i in Z]
+c, m = 'r', 'o'
+ax.scatter(xs, ys, zs, c=c, marker=m)
+# ax.plot_surface(X, Z, rstride=8, cstride=8, alpha=0.3)
+ax.plot_surface(X, Z, rstride=8, cstride=8, alpha=0.3)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
+print X,Y
+
 def ThreeDPlot():
+
     x = y = arange(-3.0, 3.0, delta)
     X, Y = meshgrid(x, y)
     pass
