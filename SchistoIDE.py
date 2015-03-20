c4ce7650524873b841c86a4bb66d06fd1cf14f35
unknown <marioharper182@gmail.com>
2015-03-20 12:38:44 -0600

    Fixed and added speed graphs for diffusion.

--- SchistoIDE.py
+++ SchistoIDE.py
@@ -1,13 +1,11 @@
 __author__ = 'mario'
 
 from numpy.fft import fft, ifft, fftshift
-from numpy import array, arange, meshgrid
-import numpy
+from numpy import array
 from scipy.stats import linregress
 import math
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
+from pylab import *
 
 # Variables definition
 np = 256
@@ -17,19 +15,20 @@ dx = 2*x1/np
 D = .01
 
 mu = 0.0002
-deltas = [0.01, .05, .1]
+delta = 0.05
 h = 100
 f = 0.00005
 b = .0005
 s = 10
-v = 0.0
+v = 0.05
 
 #Note that the Critical Value is derived from the SchistosomiasisF.m in Matlab
 Hcritlist = [.0776, .5645, 4.1368]
 # Hcrit = 2.3
 
 # Points at which the graph will draw
-c = h*f
+chemical = h*f
+chemlist = [chemical, 2* chemical, 4 * chemical]
 nsteps = 10000
 keyplot = [0, nsteps-1]
 
@@ -41,135 +40,157 @@ critvalueH = []
 CritX = array(critvalueX)
 CritH = array(critvalueH)
 
+### For Plotting Purposes, Lists not as important ###
+Count = 0
 xlist = []
+Hlist = []
 Hinitial = []
 H1 = []
 H2 = []
+H3 = []
 
-for delta in deltas:
-    count = 0
-    if delta == .01:
-        Hcrit = Hcritlist[0]
-    if delta == .05:
-        Hcrit = Hcritlist[1]
-        count = count + 1
-    if delta == .1:
-        Hcrit = Hcritlist[2]
-        count = count + 1
-    #Discretization of initial distribution
-    x = []
-    for i in range(0,np-1):
-        newrange = -x1 + i*dx
-        x.append(newrange)
-        if count == 0:
-            xlist.append(newrange)
-    x = array(x)
+for c in chemlist:
+    for delta in [0.01, .05, .1]:
+        Count = Count + 1
+        if delta == .01:
+            Hcrit = Hcritlist[0]
+        if delta == .05:
+            Hcrit = Hcritlist[1]
+        if delta == .1:
+            Hcrit = Hcritlist[2]
+        #Discretization of initial distribution
+        x = []
+        for i in range(0,np-1):
+            newrange = -x1 + i*dx
+            x.append(newrange)
+        x = array(x)
+        xlist.append(x)
 
-    I0 = array([0] * 255)
+        I0 = array([0] * 255)
 
-    H0 = []
-    H0_element = array([abs(i) for i in x])
-    for i in H0_element:
-        if i <=.25:
-            H0_i = 20 * 1
-            H0.append(H0_i)
-        else:
-            H0.append(0)
-    H0 = array(H0)
+        H0 = []
+        H0_element = array([abs(i) for i in x])
+        for i in H0_element:
+            if i <=.25:
+                H0_i = 20 * 1
+                H0.append(H0_i)
+            else:
+                H0.append(0)
+        H0 = array(H0)
 
-    K = []
-    for i in x:
-        equation = math.exp(-1*(i-v)**2/(4*D))/math.sqrt(4*math.pi*D)
-        K.append(equation)
+        K = []
+        for i in x:
+            equation = math.exp(-1*(i-v)**2/(4*D))/math.sqrt(4*math.pi*D)
+            K.append(equation)
 
-    K_new = [i/(dx*sum(K)) for i in K]
+        K_new = [i/(dx*sum(K)) for i in K]
 
-    fK = array(fft(K_new).T*dx)
-
+        fK = array(fft(K_new).T*dx)
 
 
-    for j in range(0, nsteps):
 
-        fI0 = array(fft(I0))
-        I0 = array(fftshift(ifft(fK*fI0)).real)
+        for j in range(0, nsteps):
 
-        H = (1-mu)*H0+c*I0
-        for i in H:
-            if count == 0:
-                Hinitial.append(i)
-            if count == 1:
-                H1.append(i)
-            if count == 2:
-                H2.append(i)
-        I = (1-delta)*I0+b*((s-I0)*H0**2)/(1+H0)
+            fI0 = array(fft(I0))
+            I0 = array(fftshift(ifft(fK*fI0)).real)
 
-        for i in range(128, len(x)):
-            if H[i] >= Hcrit:
-                critvalue.append((i,j))
-                critvalueH.append(j)
-                critvalueX.append(i)
+            H = (1-mu)*H0+c*I0
+            I = (1-delta)*I0+b*((s-I0)*H0**2)/(1+H0)
 
-        if j in keyplot:
-            plt.plot(x,H,'ro', linestyle = '-')
-            plt.axis([x0,x1,-1,30])
-            plt.xlabel('Distance')
-            plt.ylabel('Worms (per host)')
-            plt.title('Schistosomiasis: diffusion + advection')
-            # plt.show()
+            for i in range(128, len(x)):
+                if H[i] >= Hcrit:
+                    critvalue.append((i,j))
+                    critvalueH.append(j)
+                    critvalueX.append(i)
 
-        I0 = I
-        H0 = H
-    Instantlistx = []
-    InstantlistH = []
-    for i in range(0, len(critvalueH)):
-        if critvalueX[i] in Instantlistx:
-            pass
-        else:
-            Instantlistx.append(critvalueX[i])
-            InstantlistH.append(critvalueH[i])
+            if j in keyplot:
+                Hlist.append(H)
+                if Count == 1:
+                    Hinitial.append(H)
+                    print('Hi')
+                if Count == 2:
+                    H1.append(H)
+                    print('I am line 108, H1 append')
+                if Count == 3:
+                    H2.append(H)
+                    print('I am line 111')
+                if Count == 4:
+                    H3.append(H)
+                else:
+                    pass
+                # plt.plot(x,H,'ro', linestyle = '-')
+                # plt.axis([x0,x1,-1,30])
+                # plt.xlabel('Distance')
+                # plt.ylabel('Worms (per host)')
+                # plt.title('Schistosomiasis: diffusion + advection')
+                # plt.show()
 
-    Instantlistx = [Instantlistx[i]-128 for i in range(len(Instantlistx))]
-    # Instantlistx = array(Instantlistx)-128
-    InstantlistH = (InstantlistH)
-    print((Instantlistx,InstantlistH))
+            I0 = I
+            H0 = H
+        Instantlistx = []
+        InstantlistH = []
+        for i in range(0, len(critvalueH)):
+            if critvalueX[i] in Instantlistx:
+                pass
+            else:
+                Instantlistx.append(critvalueX[i])
+                InstantlistH.append(critvalueH[i])
 
-    plt.plot(Instantlistx, InstantlistH, 'b', linestyle = '-')
-    plt.xlabel('Spatial Coordinate of Wave Front')
-    plt.ylabel('Time to infection')
-    plt.title('speed graph')
-    plt.show()
+        Instantlistx = [Instantlistx[i]-128 for i in range(len(Instantlistx))]
+        # Instantlistx = array(Instantlistx)-128
+        InstantlistH = (InstantlistH)
+        print((Instantlistx,InstantlistH))
 
-    RegressX = Instantlistx[4:]
-    RegressY = InstantlistH[4:]
 
-    Regressionstats = linregress(RegressX,RegressY)
-    print(Regressionstats)
 
-#### 3D Plot ####
-fig = plt.figure()
-# ax = fig.gca(projection='3d')
-ax = fig.add_subplot(111, projection='3d')
+        plt.plot(Instantlistx, InstantlistH, 'b', linestyle = '-')
+        plt.xlabel('Spatial Coordinate of Wave Front')
+        plt.ylabel('Time to infection')
+        plt.title('speed graph')
+        if c == chemlist[0]:
+            if Count == 1:
+                savefig("Speed Graph Initial.png")
+            if Count == 2:
+                savefig("Speed Graph (.05).png")
+            if Count == 3:
+                savefig("Speed Graph (.1).png")
+        if c == chemlist[1]:
+            if Count == 1:
+                savefig("Speed Graph Initial c2.png")
+            if Count == 2:
+                savefig("Speed Graph (.05) c2.png")
+            if Count == 3:
+                savefig("Speed Graph (.1) c2.png")
+        if c == chemlist[2]:
+            if Count == 1:
+                savefig("Speed Graph Initial c3.png")
+            if Count == 2:
+                savefig("Speed Graph (.05) c3.png")
+            if Count == 3:
+                savefig("Speed Graph (.1) c3.png")
+        # plt.show()
 
-x = xlist
-y = deltas
+        RegressX = Instantlistx[4:]
+        RegressY = InstantlistH[4:]
 
-X,Y = meshgrid(x, y)
-Z =array([Hinitial, H1, H2])
+        Regressionstats = linregress(RegressX,RegressY)
+        print(Regressionstats)
 
-xs = [i for i in X]
-ys = [i for i in Y]
-zs = [i for i in Z]
-c, m = 'r', 'o'
-ax.scatter(xs, ys, zs, c=c, marker=m)
-# ax.plot_surface(X, Z, rstride=8, cstride=8, alpha=0.3)
-ax.plot_surface(X, Z, rstride=8, cstride=8, alpha=0.3)
-cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
-cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
-cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
-print X,Y
 
-def ThreeDPlot():
 
-    x = y = arange(-3.0, 3.0, delta)
-    X, Y = meshgrid(x, y)
-    pass
+# ### Plots of the distribution and spread of the Circuria ###
+# colors = ['r', 'g', 'b']
+# for i in range(len(xlist)):
+#     plt.figure()
+#     plt.plot(xlist[i], Hlist[i])
+#     plt.show()
+# #
+# plt.plot(xlist[0],Hinitial[0],'ro', linestyle = '-')
+# plt.plot(xlist[0],H1[0],'ro', linestyle = '-')
+# plt.plot(xlist[0],H2[0],'ro', linestyle = '-')
+# # plt.plot(x,H3[0],'ro', linestyle = '-')
+# plt.axis([x0,x1,-1,30])
+# plt.xlabel('Distance')
+# plt.ylabel('Worms (per host)')
+# plt.title('Schistosomiasis: diffusion + advection')
+# plt.show()
